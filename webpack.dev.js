da18ca7728c8e35d6e8823331898cf5654fb9893
Wilson Sheldon <wilson@namedia.net>
2025-05-20 13:28:01 -0400

    New search.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -12,9 +12,9 @@ module.exports = merge(commonConfig, {
   },
   plugins: [
     new webpack.DefinePlugin({
-      API_URL: JSON.stringify('https://catalog-service.adobe.io/graphql'),
+      API_URL: JSON.stringify('https://edge-graph.adobe.io/api/03e397ed-8ca4-4f02-b800-a441549af898/graphql'),
       TEST_URL: JSON.stringify(
-        'https://catalog-service-sandbox.adobe.io/graphql'
+        'https://edge-sandbox-graph.adobe.io/api/4de021f2-17ae-4040-8999-b86cc22eb2c8/graphql'
       ),
       SANDBOX_KEY: JSON.stringify('storefront-widgets'),
     }),
